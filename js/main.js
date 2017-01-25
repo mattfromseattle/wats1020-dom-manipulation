a0fd020c9a929c1c3dca65884185a94688e34450
Matt Dewey <mattfromseattle@me.com>
2017-01-24 18:14:41 -0800

    Updates to jQuery to show/hide content.

--- js/main.js
+++ js/main.js
@@ -3,48 +3,57 @@
 // Custom script goes here.
 //////////////////////////////////////////////////
 
-$(document).ready(function () {
-            var userInfo = {
-                firstName: 'Jane',
-                lastName: 'Doe'
-            };
-            var voteCounts = {
-                great: 0,
-                greatest: 0,
-                total: 0
-            };
-            // Place all your Javascript code inside this "document ready" function so
-            // it does not run until the DOM is ready for Javascript manipulation.
+$(document).ready(function() {
+    var userInfo = {
+        firstName: 'Jane',
+        lastName: 'Doe'
+    };
+    var voteCounts = {
+        great: 0,
+        greatest: 0,
+        total: 0
+    };
 
-            // TODO: Create a function to listen for clicks on the "login" button.
-            //      1. When a user clicks the "login" button, hide the login
-            //          form elements on the page.
-            $("#loginButton").click(function () {
-                $("#email, #password, #loginButton").hide();
-            });
-                //      2. Fill the user's first and last name into `div.user-info`.
-                //      (NOTE: You do not have to perform any validation on the data as
-                //          a base requirement.)
+    // When button is clicked, the text fields and button are faded out.
+    $("#loginButton").click(function() {
+        $("#email, #password, #loginButton").fadeOut();
+        //      2. Fill the user's first and last name into `div.user-info`.
+        //      (NOTE: You do not have to perform any validation on the data as
+        //          a base requirement.)
 
+    });
 
-                // TODO: Create a function to listen for clicks on all the "View Details"
-                // buttons so that when a user clicks a "View Details" button they see
-                // the content contained in the elements with the class "details" in the
-                // proper part of the screen.
-                //      1. When user clicks a "view details" button, find the parent of that element.
-                //      2. Within that parent, find all the elements that have the class `details`.
-                //      3. Toggle visibility of all the elements within that parent with the class `details`.
-                //      4. Change the text of the "view details" button to read "hide details" so the user
-                //          understands they can hide the text again.
+    // This looks for the button to be clicked and then expose the hidden content.
+    $('.view-details').on('click', function(event) {
+        //This will show an event with timestamp in the console.
+            console.log(event);
+            var targetElement = event.target;
+            var container = targetElement.parentElement.parentElement;
+            // This finds the 'hidden' text on the page to display.
+            $(container).find('.details').each(function(index, el){
+                //This looks to see if the content is visible. If it is, it closes it.
+                if ($(el).is(':visible')) {
+                    //This animation will slide the content to close with a set interval.
+                    $(el).slideUp(1000);
+                    //This changes the label of the button to be 'View Details'
+                    targetElement.innerText = "View Details"
+                    // If the content isn't visible, the hidden content slides down
+                } else {
+                    $(el).slideDown(1000);
+                    // Button text is updated to show as 'Hide Details'
+                    targetElement.innerText = "Hide Details"
+                }
+            })
+    });
 
-                // TODO: Create a function that listens for clicks on the voting buttons and
-                // looks at the `data-vote` attribute on each button to see what was voted for,
-                // then determines the updated vote breakdown to adjust the progress bars.
-                //      1. Set up an event listener on the buttons with the `vote` class.
-                //      2. When a button is clicked, look at the `data-vote` attribute to determine
-                //          what the user is voting for ("great" or "greatest").
-                //      3. Increment the counter for whichever vote talley is affected.
-                //      4. Determine the respective percentages (out of 100) for each progress bar.
-                //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+// TODO: Create a function that listens for clicks on the voting buttons and
+// looks at the `data-vote` attribute on each button to see what was voted for,
+// then determines the updated vote breakdown to adjust the progress bars.
+//      1. Set up an event listener on the buttons with the `vote` class.
+//      2. When a button is clicked, look at the `data-vote` attribute to determine
+//          what the user is voting for ("great" or "greatest").
+//      3. Increment the counter for whichever vote talley is affected.
+//      4. Determine the respective percentages (out of 100) for each progress bar.
+//      5. Modify the `width` attribute on each progress bar to set the updated percentage.
 
-            });
+});
